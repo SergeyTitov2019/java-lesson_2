94029a141d2e001cdc1d7683f670ac395d8a4452
serge <sergeyntitov@gmail.com>
2022-04-05 22:03:06 +0300

    lesson_06 [ 1 ]

--- src/lesson_06/Task_04.java
+++ src/lesson_06/Task_04.java
@@ -1,6 +1,8 @@
 package lesson_06;
 
 
+import java.util.Arrays;
+
 /******************************************************************************************
  *
  *          Создать двумерный массив int[][] score.
@@ -18,5 +20,25 @@ package lesson_06;
  * ****************************************************************************************/
 
 public class Task_04 {
+    public static void main(String[] args) {
+        int value = 10;
+        int count = 0;
+        int[][] num = new int[3][4];
+        int[] result = new int[num.length];
+        for (int i = 0; i < num.length; i++) {
+            count++;
+            for (int j = 0; j < num[i].length; j++) {
+                num[i][j] = i * value + j + value;
+            }
+            int[] innerArray = num[i];
+            long sum = 0;
+            for(int val : innerArray){
+                sum += val;
+            }
+            result[i]  += sum;
+            System.out.print("Array # " + count + " "+ Arrays.toString(num[i])+ "\n") ;
+        }
 
+        System.out.print("Result:" + Arrays.toString(result)+ "\n") ;
+    }
 }
