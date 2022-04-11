60c22609dff8d0e53e2a26800033780168376c2b
serge <sergeyntitov@gmail.com>
2022-04-11 19:12:36 +0300

    lesson_07 [ 0 ]

---
+++ src/lesson_07/task01/Car.java
@@ -0,0 +1,14 @@
+package lesson_07.task01;
+
+/**
+ *  Car related info
+ *
+ *  @author STitov
+ *
+ * */
+
+public class Car {
+    String carName;
+    int carPrice;
+    int amount;
+}
---
+++ src/lesson_07/task01/Client.java
@@ -0,0 +1,25 @@
+package lesson_07.task01;
+
+/**
+ *  Client related info
+ *
+ *  @author STitov
+ *
+ * */
+
+public class Client {
+    String fullName;
+    int age;
+    int sumAvailable;
+    static boolean credit;
+    static boolean sumEnough;
+
+    public static void main(String[] args) {
+        if(sumEnough ){
+            credit = true;
+        }
+
+    }
+
+
+}
