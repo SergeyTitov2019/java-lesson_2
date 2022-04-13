60bde2a4d0176bf962d2b65a9c373ef2a9536865
serge <sergeyntitov@gmail.com>
2022-04-13 23:46:51 +0300

    lesson_07 [ 0 ]

--- src/lesson_07/task01/Client.java
+++ src/lesson_07/task01/Client.java
@@ -1,5 +1,7 @@
 package lesson_07.task01;
 
+import java.time.LocalDate;
+
 /**
  *  Client related info
  *
@@ -9,10 +11,40 @@ package lesson_07.task01;
 
 public class Client {
     String fullName;
-    int age;
+    static int age;
     int sumAvailable;
     static boolean credit;
-    static boolean sumEnough;
+//    static boolean sumEnough;
+    static LocalDate day = LocalDate.now();
+
+    public static void main(String[] args) {
+        getAge(15);
+        getPurchase(true);
+
+    }
+
+    public static void getAge(int clientAge){
+        int rightYear = day.getYear() + 18 - clientAge;
+        if(clientAge < 18){
+            System.out.print("You can be our customer in " + rightYear + "\n");
+        }
+    }
+    public static void getPurchase(boolean sumEnough){
+        if(sumEnough == true){
+            credit = true;
+            System.out.print("Congrats! You can enjoy the car!" + "\n");
+        } else{
+            System.out.print("You need credit" + "\n");
+        }
+    }
+    public static void getCredit(boolean readyToCredit){
+        if(readyToCredit == true){
+            credit = true;
+            System.out.print("Congrats! You can enjoy the car!" + "\n");
+        } else{
+            System.out.print("You need credit" + "\n");
+        }
+    }
 
 
 }
