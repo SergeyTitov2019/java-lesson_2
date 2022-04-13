a0e6a5356bc12858d190ae2d4d29ad1c2119e0d3
serge <sergeyntitov@gmail.com>
2022-04-13 21:50:02 +0300

    lesson_08 [ public solution 2 ]

--- src/lesson_08/task02/GlobalRating.java
+++ src/lesson_08/task02/GlobalRating.java
@@ -1,28 +1,28 @@
 package lesson_08.task02;
 
 /**
- *      Создать класс GlobalRating со след. полями:
- *      -	Массив static Score[] data, размером 4.
- *          Этот массив нужно проинициализировать с помощью статического блока инициализации.
- *          Заполнить нужно созданными Score c категориями: views, actions, sharings, buyings
+ * Создать класс GlobalRating со след. полями:
+ * -	Массив static Score[] data, размером 4.
+ * Этот массив нужно проинициализировать с помощью статического блока инициализации.
+ * Заполнить нужно созданными Score c категориями: views, actions, sharings, buyings
  *
- *      @author SergeyTitov
- *
- * */
+ * @author SergeyTitov
+ */
 
 public class GlobalRating {
+
     public Score[] data;
 
-{
-    data = new Score[4];
-    data[0] = new Score();
-    data[1] = new Score();
-    data[2] = new Score();
-    data[3] = new Score();
-    data[0].category = "views";
-    data[1].category = "actions";
-    data[2].category = "sharings";
-    data[3].category = "buyings";
-}
+    {
+        data = new Score[4];
+        data[0] = new Score();
+        data[1] = new Score();
+        data[2] = new Score();
+        data[3] = new Score();
+        data[0].category = "views";
+        data[1].category = "actions";
+        data[2].category = "sharings";
+        data[3].category = "buyings";
+    }
 
 }
